e88dfc0c6d3a5ddfbec625c260fc1bb20e504747
nekokame3 <nesheep.dream@gmail.com>
2020-05-18 14:44:28 +0900

    initial commit

---
+++ src/app.constants.ts
@@ -0,0 +1,10 @@
+export const mongodb = 'mongodb://localhost:27017/tibetan';
+export const testdb = 'mongodb://localhost:27017/tibetantest';
+
+export const jwtConstants = {
+  secret: 'secretKey',
+  expiresIn: '6h',
+};
+
+export const TIBETAN_LETTER = '\u{0f00}-\u{0fff}';
+export const TSHEG_OR_SHAD = '\u{0f0b}-\u{0f0e}';
